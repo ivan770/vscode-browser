8bbd19d2f0e1288768db8dd306592d839a25929a
ivan770 <leshenko.ivan770@gmail.com>
2018-11-10 17:29:45 +0200

    Init code commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# VSCode Browser
+
+VSCode Browser is an integrated iframe-based web browser for Visual Studio Code.
+
+## Features
+
+Browse websites without leaving your VS Code.
+
+## Extension Settings
+
+This extension contributes the following commands:
+
+* `vscode-browser.openWindow`: Open new window
+
+## Known Issues
+
+You are not able to visit websites, that block iframe (Google).
---
+++ extension.js
@@ -0,0 +1,81 @@
+const vscode = require('vscode');
+const path = require('path');
+
+function getWebviewContent(cssFile) {
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VSCode Browser</title>
+    <link rel="stylesheet" href="${cssFile}">
+</head>
+<body>
+    <script>
+        const vscode = acquireVsCodeApi();
+        function goURL(){
+            vscode.postMessage({
+                type: 'url',
+                url: document.getElementById("url").value
+            });
+            document.getElementById("iframe-main").src = document.getElementById("url").value;
+            // document.getElementById("go").classList.add("loading");
+        }
+
+        function listenToEnter(e){
+            if (e.keyCode == 13) {
+                goURL();
+            }
+        }
+
+        /*function loadComplete(){
+            document.getElementById("go").classList.remove("loading");
+        }*/
+    </script>
+    <div class="container">
+        <div class="column col-12">
+            <div class="input-group">
+                <input type="text" class="form-input tooltip tooltip-bottom" placeholder="URL" id="url" onkeypress="listenToEnter(event)" data-tooltip="Make sure that website doesn't block iframe's">
+                <button id="go" class="btn btn-primary input-group-btn" onclick="goURL()">Go</button>
+            </div>
+            <iframe id="iframe-main" src="" style="height: 100%; width: 95.7%; position: absolute"></iframe>
+        </div>
+    </div>
+</body>
+</html>`;
+}
+
+function activate(context) {
+    context.subscriptions.push(vscode.commands.registerCommand('vscode-browser.openWindow', function () {
+        const panel = vscode.window.createWebviewPanel(
+            'browser',
+            "Empty browser window",
+            vscode.ViewColumn.One,
+            { 
+                retainContextWhenHidden: true,
+                enableScripts: true
+            }
+        );
+
+        panel.onDidDispose(null,null,context.subscriptions)
+
+        // Load CSS
+        const diskPath = vscode.Uri.file(path.join(context.extensionPath, 'assets', 'spectre.min.css'));
+        const cssFile = diskPath.with({ scheme: 'vscode-resource' });
+
+        panel.webview.onDidReceiveMessage(message => {
+            switch (message.type) {
+                case 'url':
+                    panel.title=message.url;
+                    return;
+            }
+        }, undefined, context.subscriptions);
+
+        panel.webview.html = getWebviewContent(cssFile);
+    }));
+}
+
+exports.activate = activate;
+function deactivate() {
+}
+exports.deactivate = deactivate;
